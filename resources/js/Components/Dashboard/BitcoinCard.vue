0e8271262fb910ebe6e954c30d044c36c067f84d
tharindu021 <tharinduniroshan4@gmail.com>
2024-05-07 15:25:09 +0530

    add assets to resource/src file and update components

---
+++ resources/js/Components/Dashboard/BitcoinCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card-deck">
+        <div class="card bg-gradient-danger">
+            <div class="card-body">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col">
+                        <img src="../../../src/assets/img/icons/cards/bitcoin.png" alt="Image placeholder" />
+                    </div>
+                    <div class="col-auto">
+                        <span class="badge badge-lg badge-success">Active</span>
+                    </div>
+                </div>
+                <div class="my-4">
+                    <span class="h6 surtitle text-light"> Username </span>
+                    <div class="h1 text-white">@johnsnow</div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <span class="h6 surtitle text-light">Name</span>
+                        <span class="d-block h3 text-white">John Snow</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/js/Layouts/DashboardLayout.vue
@@ -0,0 +1,90 @@
+<template>
+    <html>
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="Start your development with a Dashboard for Bootstrap 4." />
+        <meta name="author" content="Creative Tim" />
+        <title>Argon Dashboard PRO - Premium Bootstrap 4 Admin Template</title>
+
+        <link rel="canonical" href="https://www.creative-tim.com/product/argon-dashboard-pro" />
+        <meta name="keywords"
+            content="dashboard, bootstrap 4 dashboard, bootstrap 4 design, bootstrap 4 system, bootstrap 4, bootstrap 4 uit kit, bootstrap 4 kit, argon, argon ui kit, creative tim, html kit, html css template, web template, bootstrap, bootstrap 4, css3 template, frontend, responsive bootstrap template, bootstrap ui kit, responsive ui kit, argon dashboard" />
+        <meta name="description" content="Start your development with a Dashboard for Bootstrap 4." />
+
+        <meta itemprop="name" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim" />
+        <meta itemprop="description" content="Start your development with a Dashboard for Bootstrap 4." />
+        <meta itemprop="image"
+            content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg" />
+
+        <meta name="twitter:card" content="product" />
+        <meta name="twitter:site" content="@creativetim" />
+        <meta name="twitter:title" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim" />
+        <meta name="twitter:description" content="Start your development with a Dashboard for Bootstrap 4." />
+        <meta name="twitter:creator" content="@creativetim" />
+        <meta name="twitter:image"
+            content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg" />
+
+        <meta property="fb:app_id" content="655968634437471" />
+        <meta property="og:title" content="Argon - Premium Dashboard for Bootstrap 4 by Creative Tim" />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://demos.creative-tim.com/argon-dashboard/index.html" />
+        <meta property="og:image"
+            content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/opt_adp_thumbnail.jpg" />
+        <meta property="og:description" content="Start your development with a Dashboard for Bootstrap 4." />
+        <meta property="og:site_name" content="Creative Tim" />
+
+        <link rel="icon" href="../../assets/img/brand/favicon.png" type="image/png" />
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" />
+
+        
+    </head>
+
+    <body>
+        <SideBar />
+        <div class="main-content" id="panel">
+            <NavBar />
+            <slot name="header" />
+            <div class="container-fluid mt--6">
+                <slot name="content" />
+                <Footer />
+            </div>
+            
+        </div>
+        
+        <!-- <script src="../../../src/assets/vendor/jquery/dist/jquery.min.js"></script>
+        <script src="../../../src/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="../../../src/assets/vendor/js-cookie/js.cookie.js"></script>
+        <script src="../../../src/assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+        <script src="../../../src/assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+
+        <script src="../../../src/assets/vendor/chart.js/dist/Chart.min.js"></script>
+        <script src="../../../src/assets/vendor/chart.js/dist/Chart.extension.js"></script>
+
+        <script src="../../../src/assets/js/argon.min.js?v=1.2.1"></script>
+
+        <script src="../../../src/assets/js/demo.min.js"></script> -->
+    </body>
+
+    </html>
+</template>
+
+<script>
+
+import SideBar from '../Components/Dashboard/SideBar.vue';
+import NavBar from '../Components/Dashboard/NavBar.vue';
+import Footer from '../Components/Footer.vue';
+export default {
+    components: {
+        SideBar,
+        NavBar,
+        Footer,
+    }
+}
+</script>
+
+<style>
+
+</style>
